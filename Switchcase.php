c9ce0f5a961b920847b023a62665918bcbaa3fa5
Luiz Felipe <luizfelipemourab@gmail.com>
2023-07-02 10:51:46 -0300

    Add files via upload

---
+++ Switchcase.php
@@ -0,0 +1,20 @@
+<?php
+$num = 5;
+switch ($num) {  //só teste variáveis que possam ser convertidas para inteiro
+     
+    case 10 : //blocos case
+         echo "O valor é 10";
+         break;
+    
+    case 5:
+    echo "O valor é 5";
+         break;
+        
+        
+    case 0:
+    echo "O valor é 0";
+         break;
+
+
+}
+
